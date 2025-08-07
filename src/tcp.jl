e202f815176f8d1ca029f487d79cdb69314f13a7
Sole <taiyang_zhao@163.com>
2025-08-07 23:38:03 +0800

    WIP

--- src/tcp.jl
+++ src/tcp.jl
@@ -4,9 +4,10 @@ using Sockets
 const START = "<s>"
 const FINAL = "<e>"
 const ENCODING = UInt8
-
-const lstart = length(START) # 开始标记长度
-const lfinal = length(FINAL) # 结束标记长度
+const HEAD = transcode(ENCODING, START) # 文件头编码
+const TAIL = transcode(ENCODING, FINAL) # 文件尾编码
+const lstart = length(START)            # 开始标记长度
+const lfinal = length(FINAL)            # 结束标记长度
 
 
 # 定义地址和端口
@@ -18,22 +19,20 @@ port = 8000
 """
     putdata(sock::TCPSocket, data::Vector{Float32})
 
-服务端返回格式
-"<s>" + 
-nbytes::Int32 + 
-torque::Vector{Float32} + 
-speed::Vector{Float32} + 
-current::Vector{Float32} + 
-indices::Vector{Int32} + 
+服务端返回格式：
+"<s>"
+nbytes  :: Int32             # 后续所有数值数据的比特数，不包括结束标志
+torque  :: Vector{Float32}   # 转矩
+speed   :: Vector{Float32}   # 转速
+current :: Vector{Float32}   # 电流
+indices :: Vector{Int32}     # 所取电流的实际索引
 "<e>"
 """
-function putdata(sock::TCPSocket, torque::Vector{Float32},
-                                  speed::Vector{Float32},
-                                  current::Vector{Float32},
-                                  indices::Vector{Int32})
+function putdata(sock::TCPSocket, torque  :: Vector{Float32},
+                                  speed   :: Vector{Float32},
+                                  current :: Vector{Float32},
+                                  indices :: Vector{Int32})
     try
-        HEAD = transcode(ENCODING, START)
-        TAIL = transcode(ENCODING, FINAL)
         SIZE = UInt32(sizeof(torque) +
                       sizeof(speed) +
                       sizeof(current) +
@@ -55,25 +54,24 @@ end
 
 
 """
-    getdata(sock::TCPSocket) -> y::Vector{Float32}
+    getdata(sock::TCPSocket) -> y::Vector{UInt8}
 
-客户端发送格式：
-"<s>" + 
-nbytes::Int32 + 
-train_or_test::Int32 + 
-RPM::Float32 + 
-Nm::Float32 + 
-Fs::Float32 + 
-Fmax::Float32 + 
-Fmin::Float32 + 
-data::Vector{Float32} + 
-"<e>"
+客户端发来的格式：
+"<s>"   :: String
+nbytes  :: Int32           # 后续所有数值数据的比特数，不包括结束标志
+flag    :: Int32           # 训练为1，测试为0
+RPM     :: Float32         # 良品的标定转速，与标定扭矩相对应
+Nm      :: Float32         # 良品的标定扭矩(不一定是堵转扭矩)
+Fs      :: Float32         # 电流数据的采样率
+Fmax    :: Float32         # 最大转速折算的频率，由良品定义，Fmax = c * 换向片数 * 极对数 * 每分钟转速/60
+Fmin    :: Float32         # 频率下限
+data    :: Vector{Float32} # 电流数据
+"<e>"   :: String          # 数据结束标志
 """
 function getdata(sock::TCPSocket)::Vector{UInt8}
+    # 每次读取一个字节, 查找起始标记
     buffer = Vector{UInt8}()
     is_start_found = false
-
-    # 每次读取一个字节, 查找起始标记
     while !is_start_found
         push!(buffer, read(sock, UInt8))
         lbuf = length(buffer)
@@ -89,8 +87,8 @@ function getdata(sock::TCPSocket)::Vector{UInt8}
     # 读取数据的字节长度，必须是 4 的倍数
     databytes = read(sock, UInt32)
     if !iszero(databytes % 4)
-        @warn "数据长度 $databytes 不是4的倍数"
-        return UInt8[]
+        @warn "数据长度 $databytes 不是4的整数倍"
+        return Vector{UInt8}()
     end
 
     # 按字节数量读取数据
@@ -100,16 +98,46 @@ function getdata(sock::TCPSocket)::Vector{UInt8}
     mark = String(read(sock, lfinal))
     if !isequal(mark, FINAL)
         @warn "结束标记不匹配（数据可能损坏）"
-        return UInt8[]
+        return Vector{UInt8}()
     end
 
     return data
 end
 
 
+"""
+    parsedata(data::Vector{UInt8}) -> y::Vector{UInt8}
 
+flag    :: Int32           # 训练为1，测试为0
+RPM     :: Float32         # 良品的标定转速，与标定扭矩相对应
+Nm      :: Float32         # 良品的标定扭矩(不一定是堵转扭矩)
+Fs      :: Float32         # 电流数据的采样率
+Fmax    :: Float32         # 最大转速折算的频率，由良品定义，Fmax = c * 换向片数 * 极对数 * 每分钟转速/60
+Fmin    :: Float32         # 频率下限
+data    :: Vector{Float32} # 电流数据
+"""
 function parsedata(data::Vector{UInt8})
+    if isempty(data)
+        flag = one(Int32)
+        RPM  = 0f0
+        Nm   = 0f0
+        Fs   = 0f0
+        Fmax = 0f0
+        Fmin = 0f0
+        return flag,RPM,Nm,Fs,Fmax,Fmin, Vector{Float32}()
+    end
 
+    x = reinterpret(Float32, data)
+    n = length(x)
+    y = x[7:n]
 
+    flag = first( reinterpret(Int32, x[1:1]) )
+    RPM  = x[2]
+    Nm   = x[3]
+    Fs   = x[4]
+    Fmax = x[5]
+    Fmin = x[6]
 
+    return flag,RPM,Nm,Fs,Fmax,Fmin, y
 end
+
